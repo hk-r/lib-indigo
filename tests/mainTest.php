77f89fc090e2b15e280e907a8144da50c058bd8c
gushikawa <natsuki.gushikawa@imjp.co.jp>
2018-08-09 14:01:39 +0900

    [LINUX動作確認] 公開後、本番環境パスのブランチが正しいかのテストケース追加

--- tests/mainTest.php
+++ tests/mainTest.php
@@ -397,6 +397,7 @@ class mainTest extends PHPUnit_Framework_TestCase{
 		}
 		clearstatcache();
 	}
+	
 	/**
 	 * 即時公開処理処理
 	 */
@@ -423,9 +424,11 @@ class mainTest extends PHPUnit_Framework_TestCase{
 		//============================================================
 		$options = $this->options;
 
+		$branch_name = 'release/2018-04-01';
+
 		// 画面入力項目の設定
 		$options['_POST'] = array('immediate_confirm' => 1,	
-								'branch_select_value' => 'release/2018-04-01',	
+								'branch_select_value' => $branch_name,	
 								'reserve_date' => null,
 								'reserve_time' => null,	
 								'commit_hash' => 'f9fd330',	
@@ -440,9 +443,24 @@ class mainTest extends PHPUnit_Framework_TestCase{
 		// 即時公開
 		$result = $publish->exec_publish(2, null);
 
+		$path = '';
+		// 本番環境ディレクトリの絶対パスを取得。（配列1番目のサーバを設定）
+		foreach ( (array)$options['server'] as $server ) {
+			$path = $this->fs->normalize_path($this->fs->get_realpath($server['real_path'] . "/"));
+			break; // 現時点では最初の1つのみ有効なのでブレイク
+		}
+		// echo $path;
+		// echo "\n";
+		$current_branch_name = $this->get_current_branch_name($path);
+		// echo $current_branch_name;
+		// echo "\n";
+
+		$this->assertEquals( $branch_name, $current_branch_name );
+		
 		$this->assertTrue( $result['status'] );
 		$this->assertEquals( '公開処理が成功しました。', $result['message'] );
 		$this->assertTrue( isset($result['output_id']) );
+
 		// TODO:ログなどのアウトプットファイルも要確認
 		// $this->assertTrue( is_dir( __DIR__.'/testdata/indigo_dir/running/' ) )
 	}
@@ -502,5 +520,40 @@ class mainTest extends PHPUnit_Framework_TestCase{
 
 		// // ダイアログ裏で表示する初期表示画面の表示確認		
 		// $this->assertEquals( 1, count($html->find('#loader-bg div')) );
+	}
+
+
+
+	/**
+	 * 指定パス内のブランチ名取得
+	 * 
+	 * @return now_branch
+	 */
+	private function get_current_branch_name($path) {
+
+		$current_dir = realpath('.');
+
+		$now_branch = "";
+
+		// ディレクトリ移動
+		if ( $this->fs->file_exists( $path . "/.git" ) && chdir( $path ) ) {
+
+			// 現在のブランチ取得
+			exec( 'git branch', $output);
+
+			foreach ( $output as $value ) {
+				// 「*」の付いてるブランチを現在のブランチと判定
+				if ( strpos($value, '*') !== false ) {
+
+					$value = trim(str_replace("* ", "", $value));
+					$now_branch = $value;
+					break;
+				}
+			}
+		}
+
+		chdir($current_dir);
+
+		return $now_branch;		
 	}
 }
