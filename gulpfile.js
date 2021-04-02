6d2513e5995781eda0a9bfb9823578d0d6004afb
Tomoya Koyanagi <tomk79@gmail.com>
2021-04-02 14:25:14 +0900

    update libs

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,18 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');//CSSコンパイラ
-var plumber = require("gulp-plumber");//コンパイルエラーが起きても watch を抜けないようになる
-var rename = require("gulp-rename");//ファイル名の置き換えを行う
-var browserify = require("gulp-browserify");//NodeJSのコードをブラウザ向けコードに変換
-var _tasks = [
-	'client-libs',
-	'.js',
-	'.css',
-	'.css.scss'
-];
+let gulp = require('gulp');
+let sass = require('gulp-sass');//CSSコンパイラ
+let plumber = require("gulp-plumber");//コンパイルエラーが起きても watch を抜けないようになる
+let rename = require("gulp-rename");//ファイル名の置き換えを行う
+let browserify = require("gulp-browserify");//NodeJSのコードをブラウザ向けコードに変換
 
 // client-libs (frontend) を処理
-gulp.task("client-libs", function() {
+gulp.task("client-libs", function(callback) {
+	callback();
 });
 
 
 // src 中の *.js を処理
 gulp.task('.js', function(){
-	gulp.src(["src/**/*.js","!src/**/*.ignore*","!src/**/*.ignore*/*"])
+	return gulp.src(["src/**/*.js","!src/**/*.ignore*","!src/**/*.ignore*/*"])
 		.pipe(plumber())
 		.pipe(browserify({}))
 		.pipe(gulp.dest( './res/' ))
@@ -26,7 +21,7 @@ gulp.task('.js', function(){
 
 // src 中の *.css.scss を処理
 gulp.task('.css.scss', function(){
-	gulp.src(["src/**/*.css.scss","!src/**/*.ignore*","!src/**/*.ignore*/*"])
+	return gulp.src(["src/**/*.css.scss","!src/**/*.ignore*","!src/**/*.ignore*/*"])
 		.pipe(plumber())
 		.pipe(sass())
 		.pipe(rename({extname: ''}))
@@ -36,16 +31,24 @@ gulp.task('.css.scss', function(){
 
 // src 中の *.css を処理
 gulp.task('.css', function(){
-	gulp.src(["src/**/*.css","!src/**/*.ignore*","!src/**/*.ignore*/*"])
+	return gulp.src(["src/**/*.css","!src/**/*.ignore*","!src/**/*.ignore*/*"])
 		.pipe(plumber())
 		.pipe(gulp.dest( './res/' ))
 	;
 });
 
 
+let _tasks = gulp.parallel(
+	'client-libs',
+	'.js',
+	'.css',
+	'.css.scss'
+);
+
+
 // src 中のすべての拡張子を監視して処理
 gulp.task("watch", function() {
-	gulp.watch(["src/**/*"], _tasks);
+	return gulp.watch(["src/**/*"], _tasks);
 });
 
 
